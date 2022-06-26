b3e97141636111cda0938330705da032f734eafc
amecam <amecam.weinstein@gmail.com>
2022-06-26 21:37:06 +0300

    finished dailychallenge

---
+++ week2/day1/dailychallenge/script.js
@@ -0,0 +1,12 @@
+//let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+//fruits.shift();
+//fruits.sort();
+//fruits.push("kiwi");
+//fruits.splice(1,1);
+//fruits.reverse();
+
+//console.log (fruits);
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+let item = moreFruits[1][1][0];
+console.log (item);
