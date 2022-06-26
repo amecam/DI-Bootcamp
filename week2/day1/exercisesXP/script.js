594ef6e5752d5d463811286a632c29cbf4db1f13
amecam <amecam.weinstein@gmail.com>
2022-06-26 16:20:56 +0300

    finished exercisesxp

---
+++ week2/day1/exercisesXP/script.js
@@ -0,0 +1,203 @@
+//exercise1----------
+
+//let favoritefood = 'kebab';
+//let favoritemeal = 'dinner';
+//var text1 = 'i eat'
+//var text2 = 'at every'
+//var text3 = text1.concat (" ",favoritefood, " ", text2, " ", favoritemeal);
+//console.log (text3);
+
+//----------------------
+
+//exercise2-----------------
+
+//let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+//let myWatchedSeriesLength = '3';
+//let myWatchedSeriesSentence =['black mirror, money heist, and the big bang theory'];
+//console.log (`I watched ${myWatchedSeriesLength} series: ${myWatchedSeriesSentence}`);
+
+//let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+//myWatchedSeries[2] = "friends"
+
+//myWatchedSeries.push ('fauda')
+//myWatchedSeries.unshift ('familyguy')
+//myWatchedSeries.splice (1,1)
+
+//console.log (myWatchedSeries[1][2])
+
+
+
+
+
+//---------------------------------
+
+//exercise3------------------
+
+//let celsius = '27'
+//let fahrenheit = '80.6'
+//console.log (`${celsius}°C is ${fahrenheit}°F`)
+
+
+//exercise4---------------------
+
+// 	let c;
+//     let a = 34;
+//     let b = 21;
+
+//     //console.log(a+b) //first expression
+//     // Prediction:55
+//     // Actual:55
+
+//     a = 2;
+
+//     console.log(a+b) //second expression
+//     // Prediction:23
+//     // Actual:23
+
+// value of c is undefined
+
+// //console.log(3 + 4 + '5');
+// 7 and 5 joined together
+
+//----------------------------
+
+
+//exercise5-------------------
+
+
+
+// typeof(15)
+// // Prediction:number
+// // Actual:number
+
+// typeof(5.5)
+// // Prediction:number
+// // Actual:number
+
+// typeof(NaN)
+// // Prediction:number
+// // Actual:number
+
+// typeof("hello")
+// // Prediction:string
+// // Actual:string
+
+// typeof(true)
+// // Prediction:boolean
+// // Actual:boolean
+
+// typeof(1 != 2)
+// // Prediction:number
+// // Actual:boolean
+
+// "hamburger" + "s"
+// // Prediction:string
+// // Actual:hamburgers
+
+// "hamburgers" - "s"
+// // Prediction:hamburger
+// // Actual:Nan
+
+// "1" + "3"
+// // Prediction:4
+// // Actual:13
+
+// "1" - "3"
+// // Prediction:NaN
+// // Actual:-2
+
+// console.log("johnny" + 5)
+// // Prediction:johnny5
+// // Actual:johnny5
+
+// console.log("johnny" - 5)
+// // Prediction:Nan
+// // Actual:Nan
+
+// 99 * "hello"
+// // Prediction:Nan
+// // Actual:Nan
+
+// 1 != 1
+// // Prediction:false
+// // Actual:false
+
+// 1 == "1"
+// // Prediction:true
+// // Actual:true
+
+// 1 === "1"
+// // Prediction:true
+// // Actual:false
+
+
+//---------------------
+
+
+//-----exercise6-----------
+
+5 + "34"
+// Prediction:534
+// Actual:534
+
+5 - "4"
+// Prediction:1
+// Actual:1
+
+10 % 5
+// Prediction:2
+// Actual:0
+
+5 % 10
+// Prediction:5
+// Actual:5
+
+"Java" + "Script"
+// Prediction:javascript
+// Actual:javascript
+
+" " + " "
+// Prediction:"  "
+// Actual:
+
+" " + 0
+// Prediction:" " 0
+// Actual:" " 0
+
+true + true
+// Prediction:truetrue
+// Actual:2
+
+true + false
+// Prediction:0
+// Actual:1
+
+false + true
+// Prediction:1
+// Actual:1
+
+false - true
+// Prediction:-1
+// Actual:-1
+
+!true
+// Prediction:false
+// Actual:false
+
+3 - 4
+// Prediction:-1
+// Actual:-1
+
+"Bob" - "bill"
+// Prediction:Nan
+// Actual:Nan
+
+//--------------------
+
+
+
+
+
+
+
+
---
+++ week2/day1/script.js
@@ -0,0 +1,57 @@
+//let addressNumber = '4';
+//let addressStreet ='korczak';
+//let country = 'israel';
+//let globaladdress = ('addressNumber' + 'addressStreet' + 'country');
+
+
+//console.log (addressNumber + addressStreet + country);
+
+
+let birthyear = '1993';
+let futureyear = '2050'
+let futureage = '57'
+var text1 = 'i will be'
+var text2 ='in'
+var text3 = text1.concat (" ",  futureage, " ", text2, " ", futureyear);
+
+console.log (text3);
+
+
+
+	//let arr= [1,3,4,
+	//[3,5, 
+	//[7,8,9]
+	//],
+	//6];
+
+	//let arr1 = new Array();
+
+	//let a = arr[2];
+
+	//console.log(a);
+
+	//arr[7] = 8;
+	//console.log (arr[3][2][1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
