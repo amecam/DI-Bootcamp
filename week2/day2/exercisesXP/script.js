7a37c8170bfb3c3060d573a2d44e1b0f9b8979d0
amecam <amecam.weinstein@gmail.com>
2022-06-27 14:50:25 +0300

    finished exerciseXP

---
+++ week2/day2/exercisesXP/script.js
@@ -0,0 +1,70 @@
+//exercise 1-----------------------
+
+// let x = 5;
+// let y = 2;
+
+// if (x > y) {
+// 	console.log(alert('x is the biggest number'))
+// }
+// else if (x < y) {
+// 	console.log (alert('false'))
+// }
+
+//-----exercise 2-----------------------------------
+
+
+// let newDog = 'Chihuahua';
+// let n = newDog.length;
+// console.log(n)
+// console.log(newDog.toUpperCase())
+// console.log(newDog.toLowerCase())
+// if (newDog == "Chihuahua") {
+// 	console.log(alert('I love Chihuahuas, its my favorite dog breed'))
+// }
+
+// else  {
+// 	console.log (alert('I dont care i prefer cats'))
+// }
+
+
+//------exercise 3--------------------------------
+
+
+// let number = prompt('insert a number');
+
+// if (number%2==0) {
+// 	alert(`${number} is an even number`)
+// }
+// else if (number%2!=0) {
+// 	alert(`${number} is an odd number`)
+// }
+
+////---exercise 4-----------------------------
+
+// let numuser = prompt ("insert number of users")
+// let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+// let name_user1= users [0];
+// let name_user2= users [1];
+// let name_user3= numuser-2;
+
+// if (numuser==0) {
+// 	alert("no one is online")
+// }
+
+// else if (numuser==1) {
+// 	alert(`${name_user1} is online`)
+// }
+
+// else if (numuser==2) {
+// 	alert(`${name_user1} and ${name_user2} are online`)
+// }
+
+// else if (numuser>2) {
+// 	alert(`${name_user1}, ${name_user2} and ${name_user3} more are online`)
+
+// }
+
+//----------------------------------------
+
+
+
---
+++ week2/day2/script.js
@@ -0,0 +1,118 @@
+// let driver = prompt('how old are you');
+
+// if (driver < 18) {
+// 	console.log(alert('Sorry, you are too young to drive this car. Powering off')) 
+// }
+// else if (driver == 18) {
+// 	console.log(alert('Congratulations on your first year of driving. Enjoy the ride!'))
+// }
+// else if (driver > 18) {
+// 	console.log(alert('Powering On. Enjoy the ride'))
+// }
+
+//let a = 8;
+//let b = 7;
+//let c = 6;
+
+
+
+//let num = (a===b) ? 6 : 8 ;
+
+// let page = 'Home';
+
+// switch (page) {
+// 	case 'Home':
+// 	console.log ('home')
+
+// 	break;
+// 	case  'About':
+// 	console.log ('about')
+
+// 	break;
+
+// 	default:
+
+// 	console.log ('404 page');
+
+
+
+
+
+
+// }
+
+
+
+// let a = 2 + 3;
+
+// switch (a) {
+// 	case 3:
+// 	alert( 'Too small' );
+// 	break;
+// 	case 4:
+// 	alert( 'Exactly!' );
+// 	break;
+// 	case 5:
+// 	alert( 'Too large' );
+// 	break;
+// 	default:
+// 	alert( "I don't know such values" );
+// }
+
+// let a = 2 + 3;
+
+// switch (a) {
+// 	case 4:
+// 	alert('Right!');
+// 	break;
+
+//   case 3: // (*) grouped two cases
+//   case 5:
+//   alert('Wrong!');
+//   alert("Why don't you take a math class?");
+//   alert("you need to go back to grade 1");	
+//   break;
+
+//   default:
+//   alert('The result is strange. Really.');
+// }
+
+let obj = {
+
+
+	username:'amecam',
+	password:'1234',
+
+}
+
+database = [obj];
+
+
+
+newsfeed = [ 
+
+{username: 'one', timeline: 't1'},
+{username: 'two', timeline: 't2'},
+{username: 'three', timeline: 't3'}
+
+
+]
+
+console.log (database[0].password);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
